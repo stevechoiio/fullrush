397c321aa6dc5ff6a57bfe6b40b9534920769efe
Steve Choi <stevechoi93@gmail.com>
2019-04-03 14:44:41 -0700

    logout button implemented

--- client/js/screens/Account/Account.js
+++ client/js/screens/Account/Account.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, AsyncStorage } from "react-native";
 import styles from "./styles";
-export default () => {
+export default ({ navigation }) => {
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    navigation.navigate("Auth");
+  };
   return (
     <View style={styles.container}>
       <Text>Account Screen!</Text>
+      <TouchableOpacity onPress={_signOutAsync}>
+        <Text>Log Out</Text>
+      </TouchableOpacity>
     </View>
   );
 };
