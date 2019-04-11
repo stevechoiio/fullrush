0f665ce891ec703e8372ba1d920bf49b547fed2d
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-10 18:57:15 -0700

    renamed all the typos

--- client/js/screens/Washroom/WashroomContainer.js
+++ client/js/screens/Washroom/WashroomContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Washroom from "./Washroom";
-export default class RewardConatiner extends Component {
+export default class WashroomContainer extends Component {
   render() {
     return <Washroom nav={this.props.navigation} />;
   }
