e898e64613185f6e5dbf7f7cf71cf2155339d948
Steve Choi <stevechoi93@gmail.com>
2019-04-10 21:34:16 -0700

    header implemented

--- client/js/screens/Account/Account.js
+++ client/js/screens/Account/Account.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View, TouchableOpacity } from "react-native";
+import { Header } from "react-native-elements";
 import AsyncStorage from "@react-native-community/async-storage";
 import { CircularProgress } from "react-native-circular-progress";
 import styles from "./styles";
@@ -10,6 +11,12 @@ export default ({ navigation, user }) => {
   };
   return (
     <View style={styles.container}>
+      <Header
+        centerComponent={{
+          text: "My Rewards",
+          style: { color: "#fff", fontSize: 20 }
+        }}
+      />
       <View style={styles.container}>
         <Text>Reward Screen!</Text>
         <Text>Hello, {user.name}!</Text>
--- client/js/screens/AddWashroom/AddWashroom.js
+++ client/js/screens/AddWashroom/AddWashroom.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Text, View, TouchableOpacity, TextInput } from "react-native";
 import styles from "./styles";
 import { Form, Field } from "react-final-form";
-import { Button } from "react-native-elements";
+import { Button, Header } from "react-native-elements";
 import StarRating from "react-native-star-rating";
 import { graphql, compose } from "react-apollo";
 import { ADD_REVIEW, ADD_WASHROOM } from "../../config/queries";
@@ -30,130 +30,138 @@ class AddWashroom extends Component {
     let { add_washroom, add_review } = this.props;
     console.log(add_review);
     return (
-      <View style={styles.container}>
-        <Form
-          onSubmit={() => {
-            console.log("washroom submitted");
-            add_washroom({
-              variables: {
-                name: "Thompson River University",
-                stall: 5,
-                overallRating: 5,
-                toiletSeater: true
-              }
-            });
+      <View>
+        <Header
+          centerComponent={{
+            text: "Add New Washrooms",
+            style: { color: "#fff", fontSize: 20 }
           }}
-          validate={() => {}}
-          render={({
-            handleSubmit,
-            pristine,
-            invalid,
-            hasSubmitErrors,
-            submitError,
-            form
-          }) => (
-            <View style={styles.flexContent}>
-              <Text style={styles.text}>Add a Washroom</Text>
-              <Field name="email">
-                {({ input, meta }) => (
+        />
+        <View>
+          <Form
+            onSubmit={() => {
+              console.log("washroom submitted");
+              add_washroom({
+                variables: {
+                  name: "Thompson River University",
+                  stall: 5,
+                  overallRating: 5,
+                  toiletSeater: true
+                }
+              });
+            }}
+            validate={() => {}}
+            render={({
+              handleSubmit,
+              pristine,
+              invalid,
+              hasSubmitErrors,
+              submitError,
+              form
+            }) => (
+              <View style={styles.flexContent}>
+                <Text style={styles.text}>Add a Washroom</Text>
+                <Field name="email">
+                  {({ input, meta }) => (
+                    <View>
+                      <TextInput
+                        style={styles.form}
+                        editable={true}
+                        {...input}
+                        placeholder="Search the location"
+                        onChangeText={text => this.setState({ text })}
+                      />
+                      <Text style={styles.error}>
+                        {meta.error && meta.touched && meta.error}
+                      </Text>
+                    </View>
+                  )}
+                </Field>
+                <Text>How Clean was it?</Text>
+                <StarRating
+                  disabled={false}
+                  emptyStar={"ios-star-outline"}
+                  fullStar={"ios-star"}
+                  iconSet={"Ionicons"}
+                  maxStars={5}
+                  rating={this.state.starCount}
+                  selectedStar={rating => this.onStarRatingPress(rating)}
+                  fullStarColor={"black"}
+                />
+                <Text>How many stalls were there?</Text>
+                <Spinner
+                  max={10}
+                  min={0}
+                  color="#f60"
+                  value={this.state.num}
+                  numColor="black"
+                  onNumChange={num => {
+                    this.setState({ num });
+                  }}
+                />
+                <Text>were there toilet seaters?</Text>
+                {this.state.hasSeater ? (
                   <View>
-                    <TextInput
-                      style={styles.form}
-                      editable={true}
-                      {...input}
-                      placeholder="Search the location"
-                      onChangeText={text => this.setState({ text })}
+                    <Button
+                      title="👍"
+                      onPress={() => {
+                        this.setState({ hasSeater: true });
+                      }}
                     />
-                    <Text style={styles.error}>
-                      {meta.error && meta.touched && meta.error}
-                    </Text>
+                    <Button
+                      title="👎"
+                      type="clear"
+                      onPress={() => {
+                        this.setState({ hasSeater: false });
+                      }}
+                    />
+                  </View>
+                ) : (
+                  <View>
+                    <Button
+                      title="👍"
+                      type="clear"
+                      onPress={() => {
+                        this.setState({ hasSeater: true });
+                      }}
+                    />
+                    <Button
+                      title="👎"
+                      onPress={() => {
+                        this.setState({ hasSeater: false });
+                      }}
+                    />
                   </View>
                 )}
-              </Field>
-              <Text>How Clean was it?</Text>
-              <StarRating
-                disabled={false}
-                emptyStar={"ios-star-outline"}
-                fullStar={"ios-star"}
-                iconSet={"Ionicons"}
-                maxStars={5}
-                rating={this.state.starCount}
-                selectedStar={rating => this.onStarRatingPress(rating)}
-                fullStarColor={"black"}
-              />
-              <Text>How many stalls were there?</Text>
-              <Spinner
-                max={10}
-                min={0}
-                color="#f60"
-                value={this.state.num}
-                numColor="black"
-                onNumChange={num => {
-                  this.setState({ num });
-                }}
-              />
-              <Text>were there toilet seaters?</Text>
-              {this.state.hasSeater ? (
-                <View>
-                  <Button
-                    title="👍"
-                    onPress={() => {
-                      this.setState({ hasSeater: true });
-                    }}
-                  />
-                  <Button
-                    title="👎"
-                    type="clear"
-                    onPress={() => {
-                      this.setState({ hasSeater: false });
-                    }}
-                  />
-                </View>
-              ) : (
-                <View>
-                  <Button
-                    title="👍"
-                    type="clear"
-                    onPress={() => {
-                      this.setState({ hasSeater: true });
-                    }}
-                  />
-                  <Button
-                    title="👎"
-                    onPress={() => {
-                      this.setState({ hasSeater: false });
-                    }}
-                  />
-                </View>
-              )}
-              <Button
-                title="Take a photo of the washroom!"
-                type="clear"
-                onPress={() => {}}
-              />
-              <TouchableOpacity
-                onPress={async () => {
-                  console.log("washroom submitted");
-                  let washroomId = await add_washroom({
-                    variables: {
-                      name: "Thompson River University",
-                      stall: 5,
-                      overallRating: 5,
-                      toiletSeater: true
-                    }
-                  });
-                  washroomId = washroomId.data.createWashroom.id;
-                  let reviewID = await add_review({
-                    variables: { washroomId, rating: 5 }
-                  });
-                  console.log(reviewID);
-                }}
-              >
-                <Text style={styles.buttonText}>Submit!</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-        />
+                <Button
+                  title="Take a photo of the washroom!"
+                  type="clear"
+                  onPress={() => {}}
+                />
+                <TouchableOpacity
+                  onPress={async () => {
+                    console.log("washroom submitted");
+                    let washroomId = await add_washroom({
+                      variables: {
+                        name: "Thompson River University",
+                        stall: 5,
+                        overallRating: 5,
+                        toiletSeater: true
+                      }
+                    });
+                    washroomId = washroomId.data.createWashroom.id;
+                    let reviewID = await add_review({
+                      variables: { washroomId, rating: 5 }
+                    });
+                    console.log(reviewID);
+                  }}
+                >
+                  <Text style={styles.buttonText}>Submit!</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+          />
+        </View>
       </View>
     );
   }
