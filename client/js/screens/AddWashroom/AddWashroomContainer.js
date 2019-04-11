c17a1a4efd568252b5e75e1717e89205f0d1122d
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-11 10:02:21 -0700

    Fixing merge

--- client/js/screens/AddWashroom/AddWashroomContainer.js
+++ client/js/screens/AddWashroom/AddWashroomContainer.js
@@ -2,29 +2,12 @@ import React, { Component } from "react";
 import { Text } from "react-native";
 import { Drawer } from "native-base";
 import AddWashroom from "./AddWashroom";
-// const SideBar = () => {
-//   return <Text>hey</Text>;
-// };
-export default class AddWashroomContainer extends Component {
-  // closeDrawer = () => {
-  //   this.drawer._root.close();
-  // };
-  // openDrawer = () => {
-  //   this.drawer._root.open();
-  // };
 
+export default class AddWashroomContainer extends Component {
   
   render() {
     return (
-      // <Drawer
-      //   ref={ref => {
-      //     this.drawer = ref;
-      //   }}
-      //   content={<SideBar navigator={this.navigator} />}
-      //   onClose={() => this.closeDrawer()}
-      // >
         <AddWashroom nav={this.props.navigation}/>
-      // </Drawer>
     );
   }
 }
