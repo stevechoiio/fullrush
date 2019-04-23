eff08437c884f4125b416710a973d274df5ca066
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-23 15:49:15 -0700

    Fixed major workflow Bugs

--- client/js/screens/Review/Review.js
+++ client/js/screens/Review/Review.js
@@ -49,7 +49,8 @@ class Review extends Component {
       nav
     } = this.props;
     let overallRating, numberOfReviews;
-    if (washroomData) {
+    console.log(washroomData.name + "Whats this ");
+    if (washroomData.overallRating) {
       overallRating = washroomData.overallRating;
       numberOfReviews = washroomData.numberOfReviews;
     } else {
