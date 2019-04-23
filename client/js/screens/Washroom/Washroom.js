1d6d3246e09b701a82987b401b6e27a005fd5102
Steve Choi <stevechoi93@gmail.com>
2019-04-23 15:18:22 -0700

    ui changes

--- client/js/screens/Washroom/Washroom.js
+++ client/js/screens/Washroom/Washroom.js
@@ -6,20 +6,13 @@ import getDirections from "react-native-google-maps-directions";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { Header } from "react-native-elements";
 import BackButton from "../../components/BackButton";
-import {
-  Accordion,
-  List,
-  ListItem,
-  Thumbnail,
-  Left,
-  Right,
-  Body
-} from "native-base";
+import { Container } from "native-base";
+import { Col, Row, Grid } from "react-native-easy-grid";
+import { Accordion, List, ListItem, Right, Body } from "native-base";
 
 const handleGetDirections = async (lat, long) => {
   await navigator.geolocation.getCurrentPosition(pos => {
     var crd = pos.coords;
-
     const data = {
       source: {
         latitude: crd.latitude,
@@ -56,7 +49,7 @@ let checkForPhoto = item => {
 
 export default ({ reviews, refetch, data, nav }) => {
   return (
-    <View style={{ flex: 1, justifyContent: "space-between" }}>
+    <Container>
       <Header
         containerStyle={{
           backgroundColor: "#ff6b6b",
@@ -79,175 +72,159 @@ export default ({ reviews, refetch, data, nav }) => {
           </TouchableOpacity>
         }
       />
-
-      <View
-        style={{
-          alignItems: "center",
-          height: "30%",
-          justifyContent: "space-evenly"
-        }}
-      >
-        <Text style={{ ...material.caption }}>{data.address}</Text>
-        {/* <Thumbnail square source={{ uri: foo(item.listOfPhotos) }} /> */}
-        {/* <Image source={{uri: checkForPhoto(data.listOfPhotos)}}></Image> */}
-        <Thumbnail square source={{ uri: checkForPhoto(data.listOfPhotos) }} />
-        <StarRating
-          disabled={true}
-          maxStars={5}
-          rating={data.overallRating}
-          halfStarColor="#FFDF00"
-          emptyStarColor="#FFDF00"
-          fullStarColor="#FFDF00"
-        />
-        <View style={{ flex: 0, flexDirection: "row" }}>
-          <Text>Toilet Seat covers</Text>
-          <Icon name="check" size={15} color="black" />
-        </View>
-        <Text>2 stalls</Text>
-      </View>
-      <View style={{ margin: 5 }}>
-        <Text style={material.title}>Most Recent Review:</Text>
-        <ListItem>
-          <Left>
-            {reviews.allReviews[0] ? (
-              <Text style={material.caption}>
-                {reviews.allReviews[0].user.name}
-              </Text>
-            ) : (
-              <Text style={material.caption}>Bob</Text>
-            )}
-          </Left>
-          <Body>
-            <Text>not bad</Text>
-          </Body>
-          <Right>
-            <StarRating
-              disabled={true}
-              starSize={5}
-              maxStars={5}
-              rating={reviews.allReviews[0] ? reviews.allReviews[0].rating : 5}
-              halfStarColor="#FFDF00"
-              emptyStarColor="#FFDF00"
-              fullStarColor="#FFDF00"
-            />
-          </Right>
-        </ListItem>
-        <Accordion
-          renderHeader={() => {
-            return (
-              <Text style={material.subheading}>
-                see more reviews ({reviews.allReviews.length})
-              </Text>
-            );
-          }}
-          renderContent={() => {
-            return (
-              <List>
-                {reviews.allReviews ? (
-                  reviews.allReviews.map((review, index) => {
-                    return (
-                      <ListItem key={index}>
-                        <Left>
-                          {review.user ? (
-                            <Text style={material.caption}>
-                              {review.user.name}
-                            </Text>
-                          ) : null}
-                        </Left>
-                        <Body>
-                          <Text>Jesus's love is here</Text>
-                        </Body>
-                        <Right>
-                          <StarRating
-                            disabled={true}
-                            starSize={5}
-                            maxStars={5}
-                            rating={review.rating}
-                            halfStarColor="#FFDF00"
-                            emptyStarColor="#FFDF00"
-                            fullStarColor="#FFDF00"
-                          />
-                        </Right>
-                      </ListItem>
-                    );
-                  })
-                ) : (
-                  <View />
-                )}
-              </List>
-            );
-          }}
-          dataArray={[{ title: 0, content: 0 }]}
-          expanded={-1}
-        />
-        <View
-          style={{
-            alignItems: "center"
-          }}
-        />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-around"
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              margin: 3,
-              backgroundColor: "#ff6b6b",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "40%",
-              height: 50,
-              borderRadius: 13
-            }}
-            onPress={() =>
-              handleGetDirections(data.locationLat, data.locationLong)
-            }
-          >
-            <Icon name={"search-location"} size={14} color={"white"} />
-            <Text style={{ ...material.title, color: "white" }}>
-              directions
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              margin: 3,
-              backgroundColor: "#BFD7EA",
-              alignItems: "center",
-              justifyContent: "center",
-              width: "40%",
-              height: 50,
-              borderRadius: 13
-            }}
-            onPress={() => {
-              nav.navigate("Review", { refetch, data });
-            }}
-          >
-            <Icon name={"star"} size={14} color={"white"} />
-            <Text style={{ ...material.title, color: "white" }}>review</Text>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity
-          style={{
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: 30
-          }}
-          onPress={() => {}}
-        >
-          <Text
-            style={{
-              ...material.caption,
-              color: "#ff6b6b",
-              textDecorationLine: "underline"
-            }}
-          >
-            Is the infromation incorrect?
+      <Grid>
+        <Col style={{ alignItems: "center", height: "80%" }}>
+          <Text style={{ ...material.caption, marginBottom: 5 }}>
+            {data.address}
           </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+          {/* <Thumbnail square source={{ uri: foo(item.listOfPhotos) }} /> */}
+          {/* <Image source={{uri: checkForPhoto(data.listOfPhotos)}}></Image> */}
+          {/* <Thumbnail
+            square
+            source={{ uri: checkForPhoto(data.listOfPhotos) }}
+          /> */}
+          <StarRating
+            disabled={true}
+            maxStars={5}
+            rating={data.overallRating}
+            halfStarColor="#FFDF00"
+            emptyStarColor="#FFDF00"
+            fullStarColor="#FFDF00"
+          />
+          <View style={{ flex: 0, flexDirection: "row" }}>
+            <Text>Toilet Seat covers</Text>
+            <Icon name="check" size={15} color="black" />
+          </View>
+          <Text>2 stalls</Text>
+
+          <View style={{ margin: 5 }}>
+            <Text style={material.title}>Most Recent Review:</Text>
+            <ListItem>
+              <Body>
+                <Text>not bad</Text>
+              </Body>
+              <Right>
+                <StarRating
+                  disabled={true}
+                  starSize={5}
+                  maxStars={5}
+                  rating={
+                    reviews.allReviews[0] ? reviews.allReviews[0].rating : 5
+                  }
+                  halfStarColor="#FFDF00"
+                  emptyStarColor="#FFDF00"
+                  fullStarColor="#FFDF00"
+                />
+              </Right>
+            </ListItem>
+            <Accordion
+              style={{ borderColor: "white" }}
+              renderHeader={() => {
+                return (
+                  <Text style={material.subheading}>
+                    see more reviews ({reviews.allReviews.length})
+                  </Text>
+                );
+              }}
+              renderContent={() => {
+                return (
+                  <List>
+                    {reviews.allReviews &&
+                      reviews.allReviews.map((review, index) => {
+                        return (
+                          <ListItem key={index}>
+                            <Body>
+                              <Text>Jesus's love is here</Text>
+                            </Body>
+                            <Right>
+                              <StarRating
+                                disabled={true}
+                                starSize={5}
+                                maxStars={5}
+                                rating={review.rating}
+                                halfStarColor="#FFDF00"
+                                emptyStarColor="#FFDF00"
+                                fullStarColor="#FFDF00"
+                              />
+                            </Right>
+                          </ListItem>
+                        );
+                      })}
+                  </List>
+                );
+              }}
+              dataArray={[{ title: 0, content: 0 }]}
+              expanded={-1}
+            />
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+                alignItems: "center"
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  margin: 3,
+                  backgroundColor: "#ff6b6b",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "40%",
+                  height: 50,
+                  borderRadius: 13
+                }}
+                onPress={() =>
+                  handleGetDirections(data.locationLat, data.locationLong)
+                }
+              >
+                <Icon name={"search-location"} size={14} color={"white"} />
+                <Text style={{ ...material.title, color: "white" }}>
+                  directions
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  margin: 3,
+                  backgroundColor: "#BFD7EA",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  width: "40%",
+                  height: 55,
+                  borderRadius: 13
+                }}
+                onPress={() => {
+                  nav.navigate("Review", { refetch, data });
+                }}
+              >
+                <Icon name={"star"} size={14} color={"white"} />
+                <Text style={{ ...material.title, color: "white" }}>
+                  review
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                height: 40
+              }}
+              onPress={() => {}}
+            >
+              <Text
+                style={{
+                  ...material.caption,
+                  color: "#ff6b6b",
+                  textDecorationLine: "underline"
+                }}
+              >
+                Is the infromation incorrect?
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Col>
+      </Grid>
+    </Container>
   );
 };
