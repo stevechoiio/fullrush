02b9d16745513c9a435e2d11341b1698a64990c2
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-22 20:42:26 -0700

    Android Firebase Works

--- client/js/screens/AddWashroom/AddWashroom.js
+++ client/js/screens/AddWashroom/AddWashroom.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Storage } from "aws-amplify";
 import {
   Text,
   View,
@@ -64,15 +63,6 @@ class AddWashroom extends Component {
       starCount: rating
     });
   }
-
-  // TODO: Function for uploading any file/photo
-  addToStorage = () => {
-    Storage.put(this.state.photo.id, this.state.photo.uri)
-      .then(result => {
-        console.log("result: ", result);
-      })
-      .catch(err => console.log("error: ", err));
-  };
 
   render() {
     let {
