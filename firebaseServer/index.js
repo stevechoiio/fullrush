a6793933162d6694529319c57b173d965d94671d
Jinsuk Kim <jinsukkim94@gmail.com>
2019-04-22 18:51:32 -0700

    added firebase config

--- firebaseServer/index.js
+++
@@ -1,8 +0,0 @@
-import { https } from "firebase-functions";
-import setupGraphQLServer from "./graphql/server";
-
-/* CF for Firebase with graphql-server-express */
-const graphQLServer = setupGraphQLServer();
-
-// https://us-central1-<project-name>.cloudfunctions.net/api
-export const api = https.onRequest(graphQLServer);
